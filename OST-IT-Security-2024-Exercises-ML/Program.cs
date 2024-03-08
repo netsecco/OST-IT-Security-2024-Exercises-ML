2885c4d47295d6c67781f5cc7ffb114d96daa222
Thomas Gusset <thomas.gusset@netsec.co>
2024-03-08 09:11:34 +0100

    Add project files.

---
+++ OST-IT-Security-2024-Exercises-ML/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OST_IT_Security_2024_Exercises_ML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
